2c653ad4edfc650262330c06210c3ab3ea7f0528
Bartek Bachara <bbachara@poczta.onet.pl>
2024-04-14 14:55:24 +0200

    react-hw-02-feedback upd 1

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <div>
+    <div className="statistics">
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
